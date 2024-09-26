2768b14a47a0daa2bcda9a6a243e21f5a163dd75
Sumant Singh Rana <rsumantsingh@vmware.com>
2024-09-27 00:21:07 +1000

    Compilation Error

--- src/main/java/com/hkjc/springtraining/springbootrestintro/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/hkjc/springtraining/springbootrestintro/RestResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.hkjc.springtraining.springbootrestintro;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,10 +17,9 @@ import java.util.stream.Collectors;
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-        List<Error> errors = ex.getBindingResult()
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        
+            List<Error> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(fle -> new Error(fle.getField(), fle.getDefaultMessage()))
